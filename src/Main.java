2fc29da11aafb53b2016e84072ba3f2b87479d2f
Aleksei <aleksei.zh@mail.ru>
2023-11-17 13:24:21 +0300

    start

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(3, 5));
+        System.out.println(calc.mult(10, 33));
+        System.out.println(calc.pow(5, 3));
+    }
+}
